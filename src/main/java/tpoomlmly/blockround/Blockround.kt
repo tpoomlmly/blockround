0873fb65ee1e420ff7ee87233c431903d5ce26f5
Tomii Poll <tommy@poolle.co.uk>
2021-06-15 20:26:41 +0100

    add item that opens a webpage on use

--- src/main/java/tpoomlmly/blockround/Blockround.kt
+++ src/main/java/tpoomlmly/blockround/Blockround.kt
@@ -1,9 +1,6 @@
 package tpoomlmly.blockround
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.item.Item
-import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
@@ -11,8 +8,9 @@ class Blockround : ModInitializer {
 
     companion object {
         @JvmField
-        val WEB_ITEM = Item(FabricItemSettings().group(ItemGroup.MISC))
+        val WEB_ITEM = WebItem()
     }
+
     override fun onInitialize() {
         Registry.register(Registry.ITEM, Identifier("blockround", "web_item"), WEB_ITEM)
     }
---
+++ src/main/java/tpoomlmly/blockround/WebItem.kt
@@ -0,0 +1,39 @@
+package tpoomlmly.blockround
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.util.Hand
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
+
+class WebItem : Item(FabricItemSettings().group(ItemGroup.MISC)) {
+
+    companion object {
+        private val RUNTIME = Runtime.getRuntime()
+        private val OS: String = System.getProperty("os.name").lowercase()
+        private val IS_WINDOWS = "win" in OS
+        private val IS_MAC = "mac" in OS
+        private val IS_UNIX = "nix" in OS || "nux" in OS || "aix" in OS
+    }
+
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        if (world.isClient) {
+            val url = "https://fabricmc.net"
+            when {
+                IS_WINDOWS -> {
+                    RUNTIME.exec("rundll32 url.dll,FileProtocolHandler $url")
+                }
+                IS_MAC -> {
+                    RUNTIME.exec("open $url")
+                }
+                IS_UNIX -> {
+                    RUNTIME.exec("xdg-open $url")
+                }
+            }
+        }
+        return super.use(world, user, hand)
+    }
+}
