21ebd9b7b5fbeabde664abd3c41cc3e087ee88fa
Tomii Poll <tommy@poolle.co.uk>
2021-06-15 17:18:53 +0100

    initial commit
    Signed-off-by: Tomii Poll <tommy@poolle.co.uk>

---
+++ src/main/java/tpoomlmly/blockround/Blockround.kt
@@ -0,0 +1,4 @@
+package tpoomlmly.blockround
+
+class Blockround {
+}
---
+++ src/main/java/tpoomlmly/blockround/client/BlockroundClient.kt
@@ -0,0 +1,5 @@
+package tpoomlmly.blockround.client
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+class BlockroundClient {
+}
