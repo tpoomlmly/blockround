463ecd8548535ac65cbee77cc565e68950d30975
Tomii Poll <tommy@poolle.co.uk>
2021-06-22 02:04:47 +0100

    Simplify URL opening

--- src/main/java/tpoomlmly/blockround/WebItem.kt
+++ src/main/java/tpoomlmly/blockround/WebItem.kt
@@ -6,33 +6,12 @@ import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
+import net.minecraft.util.Util
 import net.minecraft.world.World
 
 class WebItem : Item(FabricItemSettings().group(Blockround.ROUND_ITEM_GROUP)) {
-
-    companion object {
-        private val RUNTIME = Runtime.getRuntime()
-        private val OS: String = System.getProperty("os.name").lowercase()
-        private val IS_WINDOWS = "win" in OS
-        private val IS_MAC = "mac" in OS
-        private val IS_UNIX = "nix" in OS || "nux" in OS || "aix" in OS
-    }
-
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        if (world.isClient) {
-            val url = "https://fabricmc.net"
-            when {
-                IS_WINDOWS -> {
-                    RUNTIME.exec("rundll32 url.dll,FileProtocolHandler $url")
-                }
-                IS_MAC -> {
-                    RUNTIME.exec("open $url")
-                }
-                IS_UNIX -> {
-                    RUNTIME.exec("xdg-open $url")
-                }
-            }
-        }
+        if (world.isClient) Util.getOperatingSystem().open("https://fabricmc.net")
         return super.use(world, user, hand)
     }
 }
